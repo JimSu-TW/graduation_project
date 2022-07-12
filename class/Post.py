0b43fe25eacdc0ca7438c3326c6803f032c37d16
JimSu <jimsu2000happy@gmail.com>
2022-07-12 10:04:09 +0800

    add Class:Post

---
+++ class/Post.py
@@ -0,0 +1,51 @@
+import datetime
+
+class Post:
+    def __init__(self, source, title, link, date, content):
+        self.setSource(source)
+        self.setTitle(title)
+        self.setLink(link)
+        self.setDate(date)
+        self.setContent(content)
+        return
+    
+    @property
+    def source(self):
+        return self._source
+    
+    @source.setter
+    def setSource(self, value):
+        self._source = value
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def setTitle(self, value):
+        self._title = value
+    
+    @property
+    def link(self):
+        return self._link
+    
+    @link.setter
+    def setLink(self, value):
+        self._link = value
+    
+    @property
+    def date(self):
+        return self._date.date.strftime("%Y/%m/%d")
+    
+    @date.setter
+    def setDate(self, value):
+        date = datetime.strptime(value, '%Y/%d/%m')
+        self._date = date
+    
+    @property
+    def content(self):
+        return self._content
+    
+    @content.setter
+    def setContent(self, value):
+        self._content = value
